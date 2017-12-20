1e73628efb5593be94252d42d722d6a1ede8a612
Ryu0823 <esm0yc1cse9rir@gmail.com>
2017-12-21 07:42:15 +0900

    add ryu0823.pythonanywhere.com as allowed hosts

--- chatui/settings.py
+++ chatui/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'n943-y-(a^d+g^(uqinrt6a!i48oz0iqp!#2tgfh1@6519^ixz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ryu0823.pythonanywhere.com']
 
 
 # Application definition
