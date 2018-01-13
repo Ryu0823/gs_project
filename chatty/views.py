4424e28c2e34a164663350b05657f9b7535addac
Ryu0823 <esm0yc1cse9rir@gmail.com>
2018-01-14 01:42:24 +0900

    changed views.post_edit to order new post first

--- chatty/views.py
+++ chatty/views.py
@@ -9,7 +9,7 @@ from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'chatty/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
